5301877e15a8992b03cf8d62d7488ec85c5c6f9b
MosRoman <morovo1988@gmail.com>
2017-07-11 13:11:11 +0300

    2

---
+++ Currency.java
@@ -0,0 +1,9 @@
+package com.gmail.morovo1988.Enum;
+
+/**
+ * Created by Дом on 03.07.2017.
+ */
+public enum Currency {
+    USD ,EUR,UAH;
+
+}
---
+++ TransactionType.java
@@ -0,0 +1,8 @@
+package com.gmail.morovo1988.Enum;
+
+/**
+ * Created by Дом on 04.07.2017.
+ */
+public enum TransactionType {
+    Transfer,Conversion,Replenish,Create;
+}
